7bedf266a2308a2c00b132c7bc28c807e542246e
Toni798 <ruiz.toni.mrm@gmail.com>
2025-01-21 01:17:04 +0100

    SSO de Google y Twitter funcionando

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '127.0.0.1', // Fuerza Vite a usar IPv4
+        port: 5173,        // Puerto del servidor de desarrollo
+    },
     plugins: [
         laravel({
-            input: ['resources/js/app.js', 'resources/css/app.css'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
     ],
